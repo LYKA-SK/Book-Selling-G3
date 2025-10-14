50c441fde96d8eb14e135195bd8fa912c31450d7
Lyka <bin.lyka@institute.pse.ngo>
2025-10-14 10:04:18 +0700

    add

---
+++ src/config/environment.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const environment = {
+  PORT: process.env.PORT || 3000,
+  MONGODB_URI:
+    process.env.MONGODB_URI ||
+    "mongodb+srv://Lyka:lyka1234567@cluster0.1ctegji.mongodb.net/",
+  JWT_SECRET: process.env.JWT_SECRET || "your-secret-key",
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1d",
+  NODE_ENV: process.env.NODE_ENV || "development",
+};
