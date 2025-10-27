e0e1068fed2e50dda8ce58dcb3d4341a15956bc2
Lyka <bin.lyka@institute.pse.ngo>
2025-10-27 16:00:15 +0700

    updet order and book

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,22 +1,3 @@
-// import express from "express";
-// import {
-//   createOrder,
-//   getAllOrders,
-//   getOrderById,
-//   updateOrderStatus,
-//   deleteOrder,
-// } from "../controllers/orderControllers";
-
-// const router = express.Router();
-
-// router.post("/", createOrder);
-// router.get("/", getAllOrders);
-// router.get("/:id", getOrderById);
-// router.patch("/:id/status", updateOrderStatus);
-// router.delete("/:id", deleteOrder);
-
-// export default router;
-
 import { Router } from "express";
 import {
   createOrder,
